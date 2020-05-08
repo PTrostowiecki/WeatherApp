b62c86e560dcc3288be7a97a1471fe4893dce4ac
PTrostowiecki <bratgollum@gmail.com>
2020-05-08 12:19:00 +0100

    First attempt to Heroku

--- src/main/java/com/myprivate/weatherapp/WeatherappApplication.java
+++ src/main/java/com/myprivate/weatherapp/WeatherappApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherappApplication extends SpringBootServletInitializer {
+public class WeatherappApplication {
 
-	@Override
+/*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
 		return app.sources(WeatherappApplication.class);
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherappApplication.class, args);
